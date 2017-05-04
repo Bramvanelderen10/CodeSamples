8eeed72e5202c40f5d47a678369dc61859df498b
Bram van Elderen <bramvanelderen1994@gmail.com>
2017-05-04 12:57:01 +0200

    Cleanup, improved and documented astar algorithm

--- Assets/CodeSamples/Astar/Astar.cs
+++ Assets/CodeSamples/Astar/Astar.cs
@@ -11,28 +11,22 @@ public class Astar : MonoBehaviour
 {
     public delegate void ActionPath(List<ANode> path);
 
-    [SerializeField] private List<string> _hitTags = new List<string>();
-    [SerializeField] private Vector3 _nodeHalfExtends;
+    [SerializeField] private List<string> _hitTags = new List<string>(); //Defines which objects are seen as obstacles
+    [SerializeField] private Vector3 _nodeHalfExtends; //Defines how big each node is
+
     private List<ANode> _nodes;
     private ANode[,] _nodesArray;
-
     private bool _isSearching = false;
+    private int _width; //Width of the grid (Columns)
+    private int _length; //Length of the grid (Rows)
+    private ActionPath _actionPath; //Is used when the path is generated
 
-    private int _width;
-    private int _length;
 
-    private ActionPath _actionPath;
-
-	// Use this for initialization
-	void Start ()
-	{
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+    /// <summary>
+    /// Generates a node grid
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="length"></param>
     public void GenerateGrid(int width, int length)
     {
         if (_isSearching)
@@ -47,9 +41,7 @@ public class Astar : MonoBehaviour
             for (int j = 0; j < width; j++)
             {
                 var node = new ANode();
-                node.Index = j + (width * i);
                 node.Position = new Vector3(j + (_nodeHalfExtends.x), transform.position.y, i + (_nodeHalfExtends.z));
-                node.Walkable = false;
                 node.gIndex = new int[2];
                 node.gIndex[0] = i;
                 node.gIndex[1] = j;
@@ -58,6 +50,12 @@ public class Astar : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Finds the position of the next node from the nearest node found
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="current"></param>
+    /// <returns></returns>
     public Vector3 FindWhereToGo(List<ANode> path, Vector3 current)
     {
         var currentNode = FindNearestNode(path, current);
@@ -69,30 +67,37 @@ public class Astar : MonoBehaviour
         return path[index].Position;
     }
 
+    /// <summary>
+    /// Initiates the algorithm
+    /// </summary>
+    /// <param name="startPosition"></param>
+    /// <param name="targetPosition"></param>
+    /// <param name="action"></param>
     public void GetPath(Vector3 startPosition, Vector3 targetPosition, ActionPath action)
     {
         if (_isSearching)
             return;
 
         _actionPath = action;
-        StartCoroutine(FindPath(.2f, FindNearestNode(startPosition), FindNearestNode(targetPosition)));
+        StartCoroutine(FindPath(FindNearestNode(startPosition), FindNearestNode(targetPosition)));
     }
 
-    IEnumerator FindPath(float maxDuration, ANode start, ANode target)
+    /// <summary>
+    /// The algorithm itself, in an ienumerator so we can split the logic over multiple frames for performance
+    /// </summary>
+    /// <param name="maxDuration"></param>
+    /// <param name="start"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    IEnumerator FindPath(ANode start, ANode target)
     {
         _isSearching = true;
         List<ANode> _openNodes = new List<ANode>();
         List<ANode> _closedNodes = new List<ANode>();
 
-        List<ANode> _testNodes = new List<ANode>();
-
-
         foreach (var node in _nodesArray)
         {
-            _testNodes.Add(node);
-            node.Walkable = false;
-            node.InOpen = false;
-            node.InClosed = false;
+            node.Available = false;
             ///Maybe use layermasks instead to simplify this loop
             var hits = Physics.OverlapBox(node.Position, _nodeHalfExtends);
             bool hit = false;
@@ -107,33 +112,36 @@ public class Astar : MonoBehaviour
             if (hit)
                 continue;
 
-            //convert index back to row and column index
-            var nodeGIndex = ConvertIndexToGridIndex(node.Index);
-            var targetGIndex = ConvertIndexToGridIndex(target.Index);
             //Calculate h value CAN BE BACKED IN GENERATEGRID
-            node.H = Mathf.Abs(nodeGIndex[1] - targetGIndex[1]) + Mathf.Abs(nodeGIndex[0] - targetGIndex[0]);
+            node.H = Mathf.Abs(node.gIndex[1] - target.gIndex[1]) + Mathf.Abs(node.gIndex[0] - target.gIndex[0]);
             node.G = 0;
-            node.Walkable = true;
+            node.Parent = null;
+            node.Available = true;
 
-            //_openNodes.Add(node);
         }
+        yield return null; //Wait a frame
         _openNodes.Add(start);
-        start.InOpen = true;
+        start.Available = false;
 
-        float waitTime = maxDuration/_nodesArray.Length;
-
+        int waitFrameInterval = Mathf.FloorToInt(_nodesArray.Length/8); //Estimated interval value to split the loop over 8 frames
+        int counter = 0;
         while (_openNodes.Count != 0)
         {
-            var current = _openNodes.Aggregate((x1, x2) => x1.F < x2.F ? x1 : x2);
-
-            if (current == target)
+            //Split loop over multiple frames
+            counter++;
+            if (counter >= waitFrameInterval)
             {
-                break;
+                counter = 0;
+                yield return null;
             }
 
-            for (int i = -1; i < 2; i++)
+            var current = _openNodes.Aggregate((x1, x2) => x1.F < x2.F ? x1 : x2); //Find the node with the lowest F value
+            if (current == target)
+                break; //Reached the target so its done
+
+            for (int i = -1; i < 2; i++) // Loop from row -1 to 1 based on the current row
             {
-                for (int j = -1; j < 2; j++)
+                for (int j = -1; j < 2; j++) // Loop from column -1 to 1 based on the current column
                 {
                     //Filter edge cases
                     if (i == -1 && current.gIndex[0] == 0)
@@ -146,9 +154,10 @@ public class Astar : MonoBehaviour
                         continue;
 
                     var node = _nodesArray[current.gIndex[0] + i,current.gIndex[1] + j];
-                    if (!node.Walkable || node.InOpen || node.InClosed)
+                    if (!node.Available)
                         continue;
 
+                    //Check if diagonal, if so more G cost
                     if (i == -1 && j == -1 ||
                         i == -1 && j == 1 ||
                         i == 1 && j == -1 ||
@@ -158,21 +167,23 @@ public class Astar : MonoBehaviour
                         node.G = current.G + 10;
 
                     node.Parent = current;
-                    node.InOpen = true;
+                    node.Available = false;
                     _openNodes.Add(node);
                 }
             }
             _openNodes.Remove(current);
-            current.InOpen = false;
-            current.InClosed = true;
             _closedNodes.Add(current);
-
-            yield return null;
         }
         _isSearching = false;
         _actionPath(RetracePath(start, target));
     }
 
+    /// <summary>
+    /// Retraces the found path based on parents
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <returns></returns>
     List<ANode> RetracePath(ANode start, ANode end)
     {
         var path = new List<ANode>();
@@ -189,6 +200,11 @@ public class Astar : MonoBehaviour
         return path;
     }
 
+    /// <summary>
+    /// Finds the nearest node of all existing nodes based on position
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
     ANode FindNearestNode(Vector3 position)
     {
         var result = _nodesArray[0, 0];
@@ -201,6 +217,12 @@ public class Astar : MonoBehaviour
         return result;
     }
 
+    /// <summary>
+    /// Finds the nearest node in a node list based on position
+    /// </summary>
+    /// <param name="nodes"></param>
+    /// <param name="position"></param>
+    /// <returns></returns>
     ANode FindNearestNode(List<ANode> nodes, Vector3 position)
     {
         var result = nodes[0];
@@ -213,6 +235,11 @@ public class Astar : MonoBehaviour
         return result;
     }
 
+    /// <summary>
+    /// Converts a regular node index to a grid index where 0 is row and 1 is column
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
     int[] ConvertIndexToGridIndex(int index)
     {
         int[] gIndex = new int[2];
@@ -223,20 +250,17 @@ public class Astar : MonoBehaviour
     }
 }
 
+/// <summary>
+/// A* node
+/// </summary>
 public class ANode
 {
-    public int Index;
     public int[] gIndex; //is an array of the row ([0]) column ([1]) index
-
-    public Vector3 Position;
-
-    public ANode Parent;
-    public float H;
-    public float G;
-    public bool Walkable;
-    public bool InOpen = true;
-    public bool InClosed = true;
-
+    public Vector3 Position; // Is used to find the nearest node and to find where to go
+    public ANode Parent; // The parenting structure used to retrace the found path
+    public float H; //The estimate from this node to target node
+    public float G; //The value required to travel to this node from start
+    public bool Available = true; //Should this node be included in the search
 
     public float F
     {
