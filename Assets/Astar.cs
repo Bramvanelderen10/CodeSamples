a447463963fa4d55434ee6ffa2e0e80b4f446c91
Bram van Elderen <bramvanelderen1994@gmail.com>
2017-05-04 04:03:30 +0200

    Astar implemented

--- Assets/Astar.cs
+++ Assets/Astar.cs
@@ -1,8 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class Astar : MonoBehaviour {
+/// <summary>
+/// Grid based astar algorithm
+/// Verical/Horizontal movement cost 10, diagonal is 14
+/// </summary>
+public class Astar : MonoBehaviour
+{
+    public delegate void ActionPath(List<ANode> path);
+
+    [SerializeField] private List<string> _hitTags = new List<string>();
+    [SerializeField] private Vector3 _nodeHalfExtends;
+    private List<ANode> _nodes;
+    private ANode[,] _nodesArray;
+
+    private bool _isSearching = false;
+
+    private int _width;
+    private int _length;
+
+    private ActionPath _actionPath;
 
 	// Use this for initialization
 	void Start ()
@@ -13,4 +32,211 @@ public class Astar : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void GenerateGrid(int width, int length)
+    {
+        _width = width;
+        _length = length;
+        _nodesArray = new ANode[length, width];
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                var node = new ANode();
+                node.Index = j + (width * i);
+                node.Position = new Vector3(j + (_nodeHalfExtends.x), transform.position.y, i + (_nodeHalfExtends.z));
+                node.Walkable = false;
+                node.gIndex = new int[2];
+                node.gIndex[0] = i;
+                node.gIndex[1] = j;
+                _nodesArray[i, j] = node;
+            }
+        }
+    }
+
+    public Vector3 FindWhereToGo(List<ANode> path, Vector3 current)
+    {
+        var currentNode = FindNearestNode(path, current);
+
+        int index = path.FindIndex(x => x == currentNode);
+        if (index < path.Count - 1)
+            index++;
+
+        return path[index].Position;
+    }
+
+    public void GetPath(Vector3 startPosition, Vector3 targetPosition, ActionPath action)
+    {
+        if (_isSearching)
+            return;
+
+        _actionPath = action;
+        StartCoroutine(FindPath(1f, FindNearestNode(startPosition), FindNearestNode(targetPosition)));
+    }
+
+    IEnumerator FindPath(float maxDuration, ANode start, ANode target)
+    {
+        _isSearching = true;
+        List<ANode> _openNodes = new List<ANode>();
+        List<ANode> _closedNodes = new List<ANode>();
+
+        List<ANode> _testNodes = new List<ANode>();
+
+
+        foreach (var node in _nodesArray)
+        {
+            _testNodes.Add(node);
+            node.Walkable = false;
+            node.InOpen = false;
+            node.InClosed = false;
+            ///Maybe use layermasks instead to simplify this loop
+            var hits = Physics.OverlapBox(node.Position, _nodeHalfExtends);
+            bool hit = false;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (_hitTags.Contains(hits[i].tag))
+                {
+                    hit = true;
+                    i = hits.Length;
+                }
+            }
+            if (hit)
+                continue;
+
+            //convert index back to row and column index
+            var nodeGIndex = ConvertIndexToGridIndex(node.Index);
+            var targetGIndex = ConvertIndexToGridIndex(target.Index);
+            //Calculate h value CAN BE BACKED IN GENERATEGRID
+            node.H = Mathf.Abs(nodeGIndex[1] - targetGIndex[1]) + Mathf.Abs(nodeGIndex[0] - targetGIndex[0]);
+            node.G = 0;
+            node.Walkable = true;
+
+            //_openNodes.Add(node);
+        }
+        _openNodes.Add(start);
+        start.InOpen = true;
+
+        float waitTime = maxDuration/_nodesArray.Length;
+
+        while (_openNodes.Count != 0)
+        {
+            var current = _openNodes.Aggregate((x1, x2) => x1.F < x2.F ? x1 : x2);
+
+            if (current == target)
+            {
+                break;
+            }
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    //Filter edge cases
+                    if (i == -1 && current.gIndex[0] == 0)
+                        continue;
+                    if (i == 1 && current.gIndex[0] == _length - 1)
+                        continue;
+                    if (j == -1 && current.gIndex[1] == 0)
+                        continue;
+                    if (j == 1 && current.gIndex[1] == _width - 1)
+                        continue;
+
+                    var node = _nodesArray[current.gIndex[0] + i,current.gIndex[1] + j];
+                    if (!node.Walkable || node.InOpen || node.InClosed)
+                        continue;
+
+                    if (i == -1 && j == -1 ||
+                        i == -1 && j == 1 ||
+                        i == 1 && j == -1 ||
+                        i == 1 && j == 1)
+                        node.G = current.G + 14;
+                    else
+                        node.G = current.G + 10;
+
+                    node.Parent = current;
+                    node.InOpen = true;
+                    _openNodes.Add(node);
+                }
+            }
+            _openNodes.Remove(current);
+            current.InOpen = false;
+            current.InClosed = true;
+            _closedNodes.Add(current);
+
+            yield return new WaitForSeconds(waitTime);
+        }
+        _actionPath(RetracePath(start, target));
+        _isSearching = false;
+    }
+
+    List<ANode> RetracePath(ANode start, ANode end)
+    {
+        var path = new List<ANode>();
+        path.Add(end);
+        while (path[path.Count - 1] != start)
+        {
+            if (path[path.Count - 1].Parent == null)
+                break;
+
+            path.Add(path[path.Count - 1].Parent);
+        }
+        path.Reverse();
+
+        return path;
+    }
+
+    ANode FindNearestNode(Vector3 position)
+    {
+        var result = _nodesArray[0, 0];
+        foreach (var node in _nodesArray)
+        {
+            if (Vector3.Distance(node.Position, position) < Vector3.Distance(result.Position, position))
+                result = node;
+        }
+
+        return result;
+    }
+
+    ANode FindNearestNode(List<ANode> nodes, Vector3 position)
+    {
+        var result = nodes[0];
+        foreach (var node in nodes)
+        {
+            if (Vector3.Distance(node.Position, position) < Vector3.Distance(result.Position, position))
+                result = node;
+        }
+
+        return result;
+    }
+
+    int[] ConvertIndexToGridIndex(int index)
+    {
+        int[] gIndex = new int[2];
+        gIndex[0] = Mathf.FloorToInt((float)index / (float)_width);
+        gIndex[1] = index - (gIndex[0] * _width);
+
+        return gIndex;
+    }
+}
+
+public class ANode
+{
+    public int Index;
+    public int[] gIndex; //is an array of the row ([0]) column ([1]) index
+
+    public Vector3 Position;
+
+    public ANode Parent;
+    public float H;
+    public float G;
+    public bool Walkable;
+    public bool InOpen = true;
+    public bool InClosed = true;
+
+
+    public float F
+    {
+        get { return H + G; }
+    }
 }
---
+++ Assets/DemoAstar.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Astar))]
+public class DemoAstar : MonoBehaviour
+{
+    [SerializeField] private GameObject _visualNodePrefab;
+
+    private Astar _astar;
+    private List<ANode> _path;
+
+    private List<GameObject> _visualPath = new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _astar = GetComponent<Astar>();
+	    GenerateGrid(15, 15);
+	    GeneratePath(new Vector3(0, 0, 0), new Vector3(12, 0, 12));
+	    DisplayPath();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DisplayPath();
+    }
+
+    public void GenerateGrid(int rows, int columns)
+    {
+        _astar.GenerateGrid(columns, rows);
+    }
+
+    public void GeneratePath(Vector3 start, Vector3 end)
+    {
+        _astar.GetPath(start, end, SavePath);
+    }
+
+    public void DisplayPath()
+    {
+        CleanUp();
+        if (_path == null)
+            return;
+        foreach (var node in _path)
+        {
+            var obj = Instantiate(_visualNodePrefab);
+            obj.transform.position = node.Position;
+            _visualPath.Add(obj);
+        }
+    }
+
+    void SavePath(List<ANode> path)
+    {
+        _path = path;
+    }
+
+    void CleanUp()
+    {
+        foreach (var item in _visualPath)
+        {
+            Destroy(item);
+        }
+    }
+}
